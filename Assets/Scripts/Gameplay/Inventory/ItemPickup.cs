04809c27f67a406f47491d95fac20aaaac1fd057
Redierro <79702421+Redierro@users.noreply.github.com>
2025-07-27 18:12:30 +0300

    Further inventory networking bug fixes

--- Assets/Scripts/Gameplay/Inventory/ItemPickup.cs
+++ Assets/Scripts/Gameplay/Inventory/ItemPickup.cs
@@ -3,17 +3,29 @@ using Mirror;
 
 public class ItemPickup : NetworkBehaviour, IInteractable
 {
-    [SerializeField] private Item itemData; // Assign in inspector
+    [SerializeField] private Item itemData;
 
     public string GetInteractionText()
         => $"Press [E] to pick up {itemData.itemName}";
 
     public void Interact(GameObject player)
-        => CmdPickupItem(player);
+        => CmdPickupItem();
+
     [Command]
-    void CmdPickupItem(GameObject player)
+    void CmdPickupItem()
     {
-        player.GetComponentInChildren<InventoryManager>().AddItem(itemData);
-        NetworkServer.Destroy(gameObject);
+        var playerIdentity = connectionToClient.identity;
+        if (playerIdentity == null)
+        {
+            Debug.LogWarning("No player identity found!");
+            return;
+        }
+
+        var inventory = playerIdentity.GetComponentInChildren<InventoryManager>();
+        if (inventory != null)
+        {
+            inventory.AddItem(itemData);
+            NetworkServer.Destroy(gameObject);
+        }
     }
 }
